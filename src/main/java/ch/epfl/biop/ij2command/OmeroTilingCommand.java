7a4df7a962231071ef1730a67d2c1b5426f9becc
Claire Stoffel <claire.stoffel@epfl.ch>
2021-03-12 11:29:25 +0100

    Split useful methods to OMERO tools

--- src/main/java/ch/epfl/biop/ij2command/OmeroTilingCommand.java
+++ src/main/java/ch/epfl/biop/ij2command/OmeroTilingCommand.java
@@ -23,13 +23,9 @@ import omero.gateway.Gateway;
 import omero.gateway.LoginCredentials;
 import omero.log.SimpleLogger;
 import org.scijava.command.Command;
-import org.scijava.platform.PlatformService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.ui.UIService;
 import org.scijava.util.VersionUtils;
-
-
 
 
 @Plugin(type = Command.class, menuPath = "Plugins>BIOP>Omero Tiling")
@@ -58,106 +54,21 @@ public class OmeroTilingCommand implements Command {
         // Run the function
         // Connect to Omero
         // https://downloads.openmicroscopy.org/omero/5.4.10/api/omero/gateway/Gateway.html
-        try {
-            Gateway gateway = omeroConnect(host, port, username, password);
-            System.out.println( "Session active : "+gateway.isConnected() );
-            openImagePlus(host,username,password,imageID);
-            System.out.println( "Disconnecting...");
-            //gateway.disconnect();
-            System.out.println( "Session active : "+gateway.isConnected() );
-        }
-        catch(Exception e) { e.printStackTrace();
-        }
+        RandomAccessibleInterval volatilerandomAccessible = OmeroTools.openRandomAccessibleInterval(host, username, password, imageID, false);
 
-        ImagePlus image = WindowManager.getCurrentImage();
-        //image.show();
-        // Read image dimensions and set total dimensions of the tiled image accordingly
-        long[] total_dim = new long[2];
-        total_dim[0] = image.getWidth()*image.getNSlices();
-        total_dim[1] = image.getHeight();
-        System.out.println("xdim : " + total_dim[0]+" ydim : " +total_dim[1]);
-
-        // Create cached image factory of Type Byte
-        ReadOnlyCachedCellImgOptions options = new ReadOnlyCachedCellImgOptions();
-        // Put cell dimensions to image width and height
-        options = options.cellDimensions(image.getWidth(),image.getHeight());
-        final ReadOnlyCachedCellImgFactory factory = new ReadOnlyCachedCellImgFactory(options);
-
-        UnsignedShortType t = new UnsignedShortType();
-
-        CellLoader<UnsignedShortType> loader = new CellLoader<UnsignedShortType>(){
-            @Override
-            public void load(SingleCellArrayImg<UnsignedShortType, ?> singleCellArrayImg) throws Exception {
-
-                ImageProcessor ip = image.getStack().getProcessor(index);
-
-                long[] positions = new long[2];
-                Cursor<UnsignedShortType> cursor = singleCellArrayImg.localizingCursor();
-
-                final long cellOffset = - (index-1)*image.getWidth();
-
-
-                // move through pixels until there is no pixel left in this cell
-                while (cursor.hasNext())
-                {
-                    // move the cursor forward by one pixel
-                    cursor.fwd();
-                    //get the current position
-                    cursor.localize(positions);
-                    long px = positions[0] + cellOffset;
-                    long py = positions[1];
-                    //get pixel value of the input image (from stack) at pos (px,py) and copy it to the current cell at the same position
-                    cursor.get().set(ip.getPixel((int) px,(int) py));
-                }
-                index = index+1;
-            }
-        };
-        RandomAccessibleInterval<UnsignedShortType> randomAccessible = factory.create(total_dim, t,loader);
-        //ask if pixel has already been loaded or not
-        RandomAccessibleInterval volatilerandomAccessible = VolatileViews.wrapAsVolatile(randomAccessible);
         BdvStackSource bss = BdvFunctions.show(volatilerandomAccessible,"Tiling");
         bss.setDisplayRange(0, 1500);
-        BdvOptions bdvOptions = new BdvOptions();
+        /*BdvOptions bdvOptions = new BdvOptions();
         bdvOptions.addTo(bss.getBdvHandle());
         AffineTransform3D transform3D = new AffineTransform3D();
         transform3D.rotate(2,Math.PI/2.0);
         bdvOptions.sourceTransform(transform3D);
         bss = BdvFunctions.show(volatilerandomAccessible,"Tiling",bdvOptions);
+        */
+
     }
 
 
-    Gateway omeroConnect(String hostname, int port, String userName, String password)throws Exception{
-        //Omero Connect with credentials and simpleLogger
-        LoginCredentials cred = new LoginCredentials();
-        cred.getServer().setHost(hostname);
-        cred.getServer().setPort(port);
-        cred.getUser().setUsername(userName);
-        cred.getUser().setPassword(password);
-        SimpleLogger simpleLogger = new SimpleLogger();
-        Gateway gateway = new Gateway(simpleLogger);
-        gateway.connect(cred);
-        return gateway;
-    }
-
-
-    void openImagePlus(String host,String username,String password,long imageID){
-        String options = "";
-        options += "location=[OMERO] open=[omero:server=";
-        options += host;
-        options += "\nuser=";
-        options += username;
-        options += "\npass=";
-        options += password;
-        //options += "\ngroupID=";
-        //options += groupID;
-        options += "\niid=";
-        options += imageID;
-        options += "]";
-        //options += " use_virtual_stack";
-        //options += " windowless=true ";
-        IJ.runPlugIn("loci.plugins.LociImporter",  options);
-        LociImporter li = new LociImporter();
-    }
 
 
     /**
--- src/main/java/ch/epfl/biop/ij2command/OmeroTools.java
+++ src/main/java/ch/epfl/biop/ij2command/OmeroTools.java
@@ -1,4 +1,131 @@
 package ch.epfl.biop.ij2command;
 
+import bdv.util.volatiles.VolatileViews;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.process.ImageProcessor;
+import loci.plugins.LociImporter;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.cache.img.CellLoader;
+import net.imglib2.cache.img.ReadOnlyCachedCellImgFactory;
+import net.imglib2.cache.img.ReadOnlyCachedCellImgOptions;
+import net.imglib2.cache.img.SingleCellArrayImg;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import omero.gateway.Gateway;
+import omero.gateway.LoginCredentials;
+import omero.gateway.SecurityContext;
+import omero.gateway.facility.BrowseFacility;
+import omero.gateway.model.ExperimenterData;
+import omero.gateway.model.ImageData;
+import omero.log.SimpleLogger;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class OmeroTools {
+
+
+    public static Gateway omeroConnect(String hostname, int port, String userName, String password)throws Exception{
+        //Omero Connect with credentials and simpleLogger
+        LoginCredentials cred = new LoginCredentials();
+        cred.getServer().setHost(hostname);
+        cred.getServer().setPort(port);
+        cred.getUser().setUsername(userName);
+        cred.getUser().setPassword(password);
+        SimpleLogger simpleLogger = new SimpleLogger();
+        Gateway gateway = new Gateway(simpleLogger);
+        gateway.connect(cred);
+        return gateway;
+    }
+
+
+
+    public static ImagePlus openImagePlus(String host, String username, String password, long imageID, boolean windowless){
+        String options = "";
+        options += "location=[OMERO] open=[omero:server=";
+        options += host;
+        options += "\nuser=";
+        options += username;
+        options += "\npass=";
+        options += password;
+        //options += "\ngroupID=";
+        //options += groupID;
+        options += "\niid=";
+        options += imageID;
+        options += "]";
+        //options += " use_virtual_stack";
+        if(windowless) {
+            options += " windowless=true ";
+        }
+        IJ.runPlugIn("loci.plugins.LociImporter",  options);
+        LociImporter li = new LociImporter();
+        ImagePlus image = WindowManager.getCurrentImage();
+        return image;
+    }
+
+
+
+
+    public static Collection<ImageData> getImagesFromDataset(Gateway gateway, long DatasetID) throws Exception{
+        //List all images contained in a Dataset
+
+        BrowseFacility browse = gateway.getFacility(BrowseFacility.class);
+        ExperimenterData exp = gateway.getLoggedInUser();
+        long groupID = exp.getGroupId();
+        SecurityContext ctx = new SecurityContext(groupID);
+        Collection<Long> datasetIds = new ArrayList<>();
+        datasetIds.add(new Long(DatasetID));
+        return browse.getImagesForDatasets(ctx, datasetIds);
+
+    }
+
+    public static RandomAccessibleInterval openRandomAccessibleInterval(String host, String username, String password, long imageID, boolean windowless){
+        ImagePlus image = OmeroTools.openImagePlus(host,username,password,imageID, windowless);
+
+        long[] total_dim = new long[2];
+        total_dim[0] = image.getWidth()*image.getNChannels();
+        total_dim[1] = image.getHeight();
+
+        // Create cached image factory of Type Byte
+        ReadOnlyCachedCellImgOptions options = new ReadOnlyCachedCellImgOptions();
+        // Put cell dimensions to image width and height
+        options = options.cellDimensions(image.getWidth(),image.getHeight());
+        final ReadOnlyCachedCellImgFactory factory = new ReadOnlyCachedCellImgFactory(options);
+
+        UnsignedShortType t = new UnsignedShortType();
+
+        CellLoader<UnsignedShortType> loader = new CellLoader<UnsignedShortType>(){
+            @Override
+            public void load(SingleCellArrayImg<UnsignedShortType, ?> singleCellArrayImg) throws Exception {
+                long[] positions = new long[2];
+                Cursor<UnsignedShortType> cursor = singleCellArrayImg.localizingCursor();
+                cursor.localize(positions);
+                System.out.println("position 0 : " + positions[0]);
+                int index = (int) ((positions[0]+1)/image.getWidth() + 1);
+                System.out.println("index : " + index);
+                ImageProcessor ip = image.getStack().getProcessor(index);
+                final long channelOffset = - (index-1)*image.getWidth();
+
+                // move through pixels until there is no pixel left in this cell
+                while (cursor.hasNext())
+                {
+                    // move the cursor forward by one pixel
+                    cursor.fwd();
+                    //get the current position
+                    cursor.localize(positions);
+                    long px = positions[0] + channelOffset;
+                    long py = positions[1];
+                    //get pixel value of the input image (from stack) at pos (px,py) and copy it to the current cell at the same position
+                    cursor.get().set(ip.getPixel((int) px,(int) py));
+                }
+            }
+
+        };
+
+        RandomAccessibleInterval<UnsignedShortType> randomAccessible = factory.create(total_dim, t,loader);
+        //ask if pixel has already been loaded or not
+        return VolatileViews.wrapAsVolatile(randomAccessible);
+    }
 }
