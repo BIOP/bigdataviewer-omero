6ed4b7b6130336d7518237246150a11290d8efc3
Claire <claire.stoffel@epfl.ch>
2021-05-05 17:32:12 +0200

    working multiresolution

--- src/main/java/ch/epfl/biop/omero/omerosource/OmeroSource.java
+++ src/main/java/ch/epfl/biop/omero/omerosource/OmeroSource.java
@@ -122,7 +122,8 @@ public abstract class OmeroSource<T extends NumericType< T >> implements Source<
 
     @Override
     public void getSourceTransform(int t, int level, AffineTransform3D transform) {
-        transform.scale(pSizeX, pSizeY,pSizeZ);
+        //TODO : change pixel size according to the resolution level
+        transform.scale(pSizeX*Math.pow(2,level), pSizeY*Math.pow(2,level),pSizeZ*Math.pow(2,level));
     }
 
     @Override
