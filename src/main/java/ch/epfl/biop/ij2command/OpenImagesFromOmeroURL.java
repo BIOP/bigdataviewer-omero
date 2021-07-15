1a644e6af0a3ef00c304a2e205f4f7233e06c8c2
Claire <claire.stoffel@epfl.ch>
2021-07-15 10:32:24 +0200

    new java class: split omero URL and new command: open omero image or dataset from a URL

---
+++ src/main/java/ch/epfl/biop/ij2command/OpenImagesFromOmeroURL.java
@@ -0,0 +1,194 @@
+package ch.epfl.biop.ij2command;
+
+import bdv.viewer.SourceAndConverter;
+import ch.epfl.biop.omero.omerosource.OmeroSourceOpener;
+import ch.epfl.biop.utils.SplitOmeroURL;
+import net.imagej.ImageJ;
+import net.imglib2.type.numeric.ARGBType;
+import omero.RType;
+import omero.cmd.CmdCallbackI;
+import omero.cmd.OriginalMetadataRequest;
+import omero.cmd.OriginalMetadataResponse;
+import omero.gateway.Gateway;
+import omero.gateway.SecurityContext;
+import omero.gateway.facility.MetadataFacility;
+import omero.gateway.model.ChannelData;
+import omero.gateway.model.ImageAcquisitionData;
+import omero.gateway.model.PixelsData;
+import omero.model.Length;
+import omero.model.PlaneInfo;
+import omero.model.enums.UnitsLength;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import sc.fiji.bdvpg.bdv.navigate.ViewerTransformAdjuster;
+import sc.fiji.bdvpg.scijava.services.SourceAndConverterBdvDisplayService;
+import sc.fiji.bdvpg.scijava.services.SourceAndConverterService;
+import sc.fiji.bdvpg.services.SourceAndConverterServices;
+import sc.fiji.bdvpg.sourceandconverter.display.BrightnessAutoAdjuster;
+import sc.fiji.bdvpg.sourceandconverter.display.ColorChanger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static ch.epfl.biop.ij2command.OmeroTools.getSecurityContext;
+import static ch.epfl.biop.utils.MetadataUtils.getRGBFromWavelength;
+
+/**
+ * Command for displaying an OMERO image (raw pixels) in 3D in BDV
+ *
+ * @parameters ("annotation") : ImageJ input parameters declaration
+ */
+@Plugin(type = Command.class, menuPath = "Plugins>BIOP>open OMERO images from URL")
+public class OpenImagesFromOmeroURL implements Command {
+
+    @Parameter(label = "OMERO Image or Dataset URL")
+    String omeroURL;
+
+    @Parameter(label = "Enter your gaspar username")
+    String username;
+
+    @Parameter(label = "Enter your gaspar password", style = "password", persist = false)
+    String password;
+
+    @Parameter
+    boolean displayInSpace;
+
+    @Parameter
+    boolean autocontrast;
+
+    @Parameter
+    boolean show;
+
+    static int port = 4064;
+
+
+    /**
+     * Command Output
+     */
+    @Parameter(type = ItemIO.OUTPUT)
+    SourceAndConverter[] sacs;
+
+    /**
+     * BDV Service for managing all BDV Sources
+     * https://github.com/bigdataviewer/bigdataviewer-playground
+     */
+    @Parameter
+    SourceAndConverterService sacService;
+
+    /**
+     * BDV Service for display
+     * https://github.com/bigdataviewer/bigdataviewer-playground
+     */
+    @Parameter
+    SourceAndConverterBdvDisplayService sacDisplayService;
+
+    @Override
+    public void run() {
+        try {
+            SplitOmeroURL splitOmeroURL = new SplitOmeroURL()
+                    .setHost(omeroURL);
+            String host = splitOmeroURL.host;
+            System.out.println(host);
+
+            Gateway gateway =  OmeroTools.omeroConnect(host, port, username, password);
+            System.out.println( "Session active : "+gateway.isConnected() );
+            SecurityContext ctx = getSecurityContext(gateway);
+
+            splitOmeroURL.setImageIDs(omeroURL,gateway,ctx);
+
+            for(long imageID : splitOmeroURL.imageIDs) {
+                System.out.println("New image from Dataset : ID " + imageID);
+
+                //create a new opener and modify it
+                OmeroSourceOpener opener = new OmeroSourceOpener()
+                        .imageID(imageID)
+                        .gateway(gateway)
+                        .securityContext(ctx)
+                        .micrometer()
+                        .displayInSpace(displayInSpace)
+                        .create();
+
+                //(new Gson()).toJson(opener);
+                //System.out.println(new Gson().toJson(opener));
+
+                //
+                sacs = new SourceAndConverter[opener.getSizeC()];
+
+                for (int c = 0; c < opener.getSizeC(); c++) {
+                    // create the right source and convertor depending on the image type
+                    sacs[c] = opener.getSourceAndConvertor(c);
+                }
+
+                // give the sources to the sacService (BDV source manager)
+                for (SourceAndConverter sac : sacs) {
+                    sacService.register(sac);
+                }
+
+                List<ChannelData> channelMetadata = opener.getChannelMetadata();
+                for (int i = 0; i < sacs.length; i++) {
+                    Length emWv = channelMetadata.get(i).getEmissionWavelength(UnitsLength.NANOMETER);
+                    Length exWv = channelMetadata.get(i).getExcitationWavelength(UnitsLength.NANOMETER);
+
+                    //If EmissionWavelength (or ExcitationWavelength) is contained in the image metadata, convert it to RGB colors for the different channels
+                    //Otherwise, put red, green and blue
+                    if (emWv != null) {
+                        new ColorChanger(sacs[i], getRGBFromWavelength((int) emWv.getValue())).run();
+                    } else if (exWv != null) {
+                        new ColorChanger(sacs[i], getRGBFromWavelength((int) exWv.getValue())).run();
+                    } else {
+                        //new ColorChanger(sacs[i], new ARGBType(ARGBType.rgba(255*(1-(i%3)), 255*(1-((i+1)%3)), 255*(1-((i+2)%3)), 255 ))).run();
+                        //If no emission nor excitation colors, display RGB
+                        new ColorChanger(sacs[i], new ARGBType(ARGBType.rgba(255 * (Math.ceil(((i + 2) % 3) / 2)), 255 * (Math.ceil(((i + 1) % 3) / 2)), 255 * (Math.ceil(((i + 3) % 3) / 2)), 255))).run();
+                    }
+                    //handle autocontrast option
+                    if (autocontrast) {
+                        new BrightnessAutoAdjuster(sacs[i], 0).run();
+                    }
+                }
+            }
+
+            //handle show option
+            if (show) {
+                SourceAndConverterServices.getBdvDisplayService().show(sacDisplayService.getActiveBdv(), sacs);
+                //adjust the viewing window in BDV to the image
+                (new ViewerTransformAdjuster(sacDisplayService.getActiveBdv(), this.sacs[0])).run();
+            }
+
+
+            // End of session
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                //fail
+                System.out.println( "Session active : "+gateway.isConnected() );
+                gateway.disconnect();
+                System.out.println("Gateway disconnected");
+            }));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+
+        ij.command().run(OpenImagesFromOmeroURL.class, true).get();
+
+
+    }
+
+}
--- src/main/java/ch/epfl/biop/utils/SplitOmeroURL.java
+++ src/main/java/ch/epfl/biop/utils/SplitOmeroURL.java
@@ -1,19 +1,80 @@
 package ch.epfl.biop.utils;
 
-import org.scijava.plugin.Parameter;
+
+import omero.gateway.Gateway;
+import omero.gateway.SecurityContext;
+import omero.gateway.exception.DSAccessException;
+import omero.gateway.exception.DSOutOfServiceException;
+import omero.gateway.facility.BrowseFacility;
+import omero.gateway.model.ImageData;
+import omero.model.Image;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 public class SplitOmeroURL {
     /*
     Dataset:
 
     https://omero-poc.epfl.ch/webclient/?show=dataset-1111
-    Image:
-    https://omero-poc.epfl.ch/webclient/img_detail/3713/
     or
-    https://omero-poc.epfl.ch/webclient/?show=image-3713
+    omero-poc.epfl.ch/webclient/?show=dataset-1111
+    Image:
+    https://omero-poc.epfl.ch/webclient/img_detail/3713/ (omero-poc.epfl.ch/webclient/img_detail/3713/)
+    or
+    https://omero-poc.epfl.ch/webclient/?show=image-3713 (omero-poc.epfl.ch/webclient/?show=image-3713)
 
      */
 
     public String host;
-    public long imageID;
+    public List<Long> imageIDs = new ArrayList<>();
+
+    public int keywordIsMemberOf(String[] array, String pattern){
+        int idx = 0;
+        for(String content : array){
+            if(StringUtils.contains(content, pattern)){
+                return idx;
+            }
+            ++idx;
+        }
+        return -1;
+    }
+
+    // retrieve Omero host
+    public SplitOmeroURL setHost(String OmeroURL) {
+        String[] parts = OmeroURL.split("/");
+        if(OmeroURL.contains("//")){
+            this.host = parts[2];
+        } else {
+            this.host = parts[0];
+        }
+        return this;
+    }
+
+    // retrieve image IDs from dataset or image URL
+    public SplitOmeroURL setImageIDs(String OmeroURL, Gateway gateway, SecurityContext ctx) throws ExecutionException, DSOutOfServiceException, DSAccessException {
+        if(OmeroURL.contains("?show=dataset-")){
+            String[] parts = OmeroURL.split("-");
+            long datasetID = Long.valueOf(parts[parts.length-1]);
+            BrowseFacility browse = gateway.getFacility(BrowseFacility.class);
+            Collection<ImageData> images = browse.getImagesForDatasets(ctx, Arrays.asList(datasetID));
+            Iterator<ImageData> j = images.iterator();
+            ImageData image;
+            while (j.hasNext()) {
+                image = j.next();
+                this.imageIDs.add(image.getId());
+            }
+        } else if (OmeroURL.contains("?show=image-")){
+            String[] parts = OmeroURL.split("-");
+            this.imageIDs.add(Long.valueOf(parts[parts.length-1]));
+        } else if (OmeroURL.contains("img_detail")){
+            String[] parts = OmeroURL.split("/");
+            this.imageIDs.add(Long.valueOf(parts[parts.length-1]));
+        } else {
+            System.out.println("Invalid OMERO URL");
+        }
+        return this;
+    }
+
 }
